198328be0dfca02582deaa828904e285ecaf7a03
Lukas <lukazelt@gmail.com>
2026-02-11 11:47:58 +0200

    Added random task assigner with challange + fallback

--- Lab1.app/obj/Debug/net10.0/Lab1.app.AssemblyInfo.cs
+++ Lab1.app/obj/Debug/net10.0/Lab1.app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2d47670e3dc5d74e1c866638d973713473cc73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e1f058264feb250a79c7b3533c935d99302ab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
